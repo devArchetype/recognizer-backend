105a3d85517c2e60ee3bc66bf16fa535095da2d7
Isaac Santiago <eoisaacc@gmail.com>
2022-11-05 17:37:24 -0300

    :heavy_plus_sign: build: configure project basic dependencies

---
+++ src/main/java/com/devarchetype/templatebrokerbackend/TemplateBrokerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devarchetype.templatebrokerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBrokerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateBrokerBackendApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/devarchetype/templatebrokerbackend/TemplateBrokerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devarchetype.templatebrokerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBrokerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
