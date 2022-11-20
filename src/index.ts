01b5cd688f098b8bae7cf570579fb99f8abeb7fc
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-11-20 15:57:40 -0300

    :wrench: chore: add prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 
-import User from '@models/User';
-
-new User().log();
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
