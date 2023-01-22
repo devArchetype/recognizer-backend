8ffe7b85cf2c78eb659085b5606c32ee686e2200
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-01-22 14:50:38 -0300

    :sparkles: feat: implement recaptcha middleware

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 
 import { authMiddleware } from '@middlewares/authMiddleware';
+import { recaptchaMiddleware } from '@middlewares/recaptchaMiddleware';
 import UserController from '@controllers/UserController';
 
 const router = express.Router();
@@ -8,7 +9,7 @@ const router = express.Router();
 // * user routes
 const userController = new UserController();
 router.post('/user/store', userController.store.bind(userController));
-router.post('/user/login', userController.login.bind(userController));
+router.post('/user/login', recaptchaMiddleware, userController.login.bind(userController));
 
 router.use(authMiddleware);
 router.delete('/user/delete', userController.delete.bind(userController));
