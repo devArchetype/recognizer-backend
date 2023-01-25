38647d8012a138a8a101432f32cd381832acd24b
Isaac Santiago <eoisaacc@gmail.com>
2023-01-25 20:35:59 -0300

    :recycle: refactor: group controller to use user data from auth

--- src/app/controllers/GroupController.ts
+++ src/app/controllers/GroupController.ts
@@ -1,22 +1,23 @@
-import { Request, Response } from 'express';
 import GroupBuilder from '@builders/GroupBuilder';
 import Group from '@entities/Group';
 import { BadRequestError, NotFoundError } from '@erros/api-erros';
+import { strictEquals } from '@helpers/stringHelpers';
 import ControllerProtocol from '@interfaces/controller.protocol';
 import { Prisma } from '@prisma/client';
-import { strictEquals } from '@helpers/stringHelpers';
+import { Request, Response } from 'express';
 
 export default class GroupController implements ControllerProtocol {
   private groupBuilder = new GroupBuilder();
 
   public async store(request: Request, response: Response): Promise<void> {
-    const { name, userId } = request.body;
+    const { name } = request.body;
+    const { id } = request.user
 
     this.groupBuilder.reset();
     this.groupBuilder.name = name ?? '';
-    this.groupBuilder.userId = userId ?? '';
+    this.groupBuilder.userId = id ?? '';
 
-    const storedGroup = await Group.findOne({ name, userId });
+    const storedGroup = await Group.findOne({ name, userId: id });
     if (storedGroup) {
       const equalNames = strictEquals(storedGroup.name, name);
       if (equalNames) {
@@ -38,7 +39,8 @@ export default class GroupController implements ControllerProtocol {
   }
 
   public async update(request: Request, response: Response): Promise<void> {
-    const { id, userId, name } = request.body;
+    const { id, name } = request.body;
+    const userId = request.user.id
 
     this.groupBuilder.reset();
     this.groupBuilder.name = name;
@@ -59,8 +61,9 @@ export default class GroupController implements ControllerProtocol {
 
   public async index(request: Request, response: Response): Promise<void> {
     const { id } = request.body;
+    const userId = request.user.id
 
-    const storedGroup = await Group.findOne({ id });
+    const storedGroup = await Group.findOne({ id, userId });
     if (!storedGroup) {
       throw new BadRequestError(
         'Oops, Algo de errado aconteceu, tente novamente mais tarde!',
@@ -74,12 +77,12 @@ export default class GroupController implements ControllerProtocol {
   }
 
   public async show(request: Request, response: Response): Promise<void> {
-    const { userId } = request.body;
+    const { id } = request.user
 
-    const storedGroups = await Group.findMany({ userId });
+    const storedGroups = await Group.findMany({ userId: id });
     if (!storedGroups) {
       throw new BadRequestError(
-        'Oops, Algo de errado aconteceu, tente novamente mais tarde!',
+        'Oops, Algo de errado aconteceu, tente novamente mais tarde!'
       );
     }
 
@@ -93,8 +96,10 @@ export default class GroupController implements ControllerProtocol {
     const { id } = request.body;
 
     try {
-      const deletedGroup = await Group.destroy({ id });
+      const deletedGroup = await Group.destroy({ id});
+
     } catch (error) {
+      console.log(error);
       if (
         error instanceof Prisma.PrismaClientKnownRequestError
         && error.code === 'P2025'
