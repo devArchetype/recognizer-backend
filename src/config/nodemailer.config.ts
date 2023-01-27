df90c5333b7027c7c1c8e1257d27c2d0895bceaf
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-01-26 22:31:57 -0300

    :wrench: chore: add nodemailer settings

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -1,7 +1,13 @@
 import * as nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 export default transporter;
